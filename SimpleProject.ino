3ee2a5f7a10496dcfe3b2cd52257d18f0972fd85
Fercosmicking <00juanferareiza@gmail.com>
2022-10-11 17:42:29 -0500

    Miguel no me quiere regalar una camisa

--- SimpleProject.ino
+++ SimpleProject.ino
@@ -1,66 +1,153 @@
-#include <Wire.h>  // Wire library - used for I2C communication
-
-int ADXL345 = 0x53; // The ADXL345 sensor I2C address
-
-float X_out, Y_out, Z_out;  // Outputs
-float roll,pitch,rollF,pitchF=0;
-
-void setup() {
-  Serial.begin(9600); // Initiate serial communication for printing the results on the Serial monitor
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
  
-  Wire.begin(); // Initiate the Wire library
-  // Set ADXL345 in measuring mode
-  Wire.beginTransmission(ADXL345); // Start communicating with the device
-  Wire.write(0x2D); // Access/ talk to POWER_CTL Register - 0x2D
-  // Enable measurement
-  Wire.write(8); // Bit D3 High for measuring enable (8dec -> 0000 1000 binary)
-  Wire.endTransmission();
-  delay(10);
-
-  //Off-set Calibration
-  //X-axis
-  Wire.beginTransmission(ADXL345);
-  Wire.write(0x1E);
-  Wire.write(1);
-  Wire.endTransmission();
-  delay(10);
-  //Y-axis
-  Wire.beginTransmission(ADXL345);
-  Wire.write(0x1F);
-  Wire.write(-2);
-  Wire.endTransmission();
-  delay(10);
-
-  //Z-axis
-  Wire.beginTransmission(ADXL345);
-  Wire.write(0x20);
-  Wire.write(-9);
-  Wire.endTransmission();
-  delay(10);
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+ 
+void displaySensorDetails(void)
+{
+sensor_t sensor;
+accel.getSensor(&sensor);
+Serial.println("------------------------------------");
+Serial.print ("Sensor: "); Serial.println(sensor.name);
+Serial.print ("Driver Ver: "); Serial.println(sensor.version);
+Serial.print ("Unique ID: "); Serial.println(sensor.sensor_id);
+Serial.print ("Max Value: "); Serial.print(sensor.max_value); Serial.println(" m/s^2");
+Serial.print ("Min Value: "); Serial.print(sensor.min_value); Serial.println(" m/s^2");
+Serial.print ("Resolution: "); Serial.print(sensor.resolution); Serial.println(" m/s^2");
+Serial.println("------------------------------------");
+Serial.println("");
+delay(500);
 }
-
-void loop() {
-  // === Read acceleromter data === //
-  Wire.beginTransmission(ADXL345);
-  Wire.write(0x32); // Start with register 0x32 (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(ADXL345, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
-  X_out = ( Wire.read() | Wire.read() << 8); // X-axis value
-  X_out = X_out / 256; //For a range of +-2g, we need to divide the raw values by 256, according to the datasheet
-  Y_out = ( Wire.read() | Wire.read() << 8); // Y-axis value
-  Y_out = Y_out / 256;
-  Z_out = ( Wire.read() | Wire.read() << 8); // Z-axis value
-  Z_out = Z_out / 256;
-
-  // Calculate Roll and Pitch (rotation around X-axis, rotation around Y-axis)
-  roll = atan(Y_out / sqrt(pow(X_out, 2) + pow(Z_out, 2))) * 180 / PI;
-  pitch = atan(-1 * X_out / sqrt(pow(Y_out, 2) + pow(Z_out, 2))) * 180 / PI;
-
-  // Low-pass filter
-  rollF = 0.94 * rollF + 0.06 * roll;
-  pitchF = 0.94 * pitchF + 0.06 * pitch;
-
-  Serial.print(rollF);
-  Serial.print("/");
-  Serial.println(pitchF);
+ 
+void displayDataRate(void)
+{
+Serial.print ("Data Rate: ");
+ 
+switch(accel.getDataRate())
+{
+case ADXL345_DATARATE_3200_HZ:
+Serial.print ("3200 ");
+break;
+case ADXL345_DATARATE_1600_HZ:
+Serial.print ("1600 ");
+break;
+case ADXL345_DATARATE_800_HZ:
+Serial.print ("800 ");
+break;
+case ADXL345_DATARATE_400_HZ:
+Serial.print ("400 ");
+break;
+case ADXL345_DATARATE_200_HZ:
+Serial.print ("200 ");
+break;
+case ADXL345_DATARATE_100_HZ:
+Serial.print ("100 ");
+break;
+case ADXL345_DATARATE_50_HZ:
+Serial.print ("50 ");
+break;
+case ADXL345_DATARATE_25_HZ:
+Serial.print ("25 ");
+break;
+case ADXL345_DATARATE_12_5_HZ:
+Serial.print ("12.5 ");
+break;
+case ADXL345_DATARATE_6_25HZ:
+Serial.print ("6.25 ");
+break;
+case ADXL345_DATARATE_3_13_HZ:
+Serial.print ("3.13 ");
+break;
+case ADXL345_DATARATE_1_56_HZ:
+Serial.print ("1.56 ");
+break;
+case ADXL345_DATARATE_0_78_HZ:
+Serial.print ("0.78 ");
+break;
+case ADXL345_DATARATE_0_39_HZ:
+Serial.print ("0.39 ");
+break;
+case ADXL345_DATARATE_0_20_HZ:
+Serial.print ("0.20 ");
+break;
+case ADXL345_DATARATE_0_10_HZ:
+Serial.print ("0.10 ");
+break;
+default:
+Serial.print ("???? ");
+break;
+}
+Serial.println(" Hz");
+}
+ 
+void displayRange(void)
+{
+Serial.print ("Range: +/- ");
+ 
+switch(accel.getRange())
+{
+case ADXL345_RANGE_16_G:
+Serial.print ("16 ");
+break;
+case ADXL345_RANGE_8_G:
+Serial.print ("8 ");
+break;
+case ADXL345_RANGE_4_G:
+Serial.print ("4 ");
+break;
+case ADXL345_RANGE_2_G:
+Serial.print ("2 ");
+break;
+default:
+Serial.print ("?? ");
+break;
+}
+Serial.println(" g");
+}
+ 
+void setup(void)
+{
+Serial.begin(115200);
+Serial.println("Accelerometer Test"); Serial.println("");
+ 
+/* Initialise the sensor */
+if(!accel.begin())
+{
+/* There was a problem detecting the ADXL345 ... check your connections */
+Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+while(1);
+}
+ 
+/* Set the range to whatever is appropriate for your project */
+accel.setRange(ADXL345_RANGE_16_G);
+// displaySetRange(ADXL345_RANGE_8_G);
+// displaySetRange(ADXL345_RANGE_4_G);
+// displaySetRange(ADXL345_RANGE_2_G);
+ 
+/* Display some basic information on this sensor */
+displaySensorDetails();
+ 
+/* Display additional settings (outside the scope of sensor_t) */
+displayDataRate();
+displayRange();
+Serial.println("");
+}
+ 
+void loop(void)
+{
+/* Get a new sensor event */
+sensors_event_t event;
+accel.getEvent(&event);
+ 
+/* Display the results (acceleration is measured in m/s^2) */
+//Serial.print("X: ");
+Serial.print(event.acceleration.x); Serial.print(",");
+//Serial.print("Y: ");
+Serial.print(event.acceleration.y); Serial.print(",");
+//Serial.print("Z: ");
+Serial.print(event.acceleration.z); 
+Serial.print("\n");
+delay(125);
 }
