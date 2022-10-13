4e69d2dcaa81265ff8f243e3702065d41e320808
Fercosmicking <00juanferareiza@gmail.com>
2022-10-13 17:33:47 -0500

    Seguimiento Unity

--- Evaluacion 3 SF/Assets/Script/App.cs
+++ Evaluacion 3 SF/Assets/Script/App.cs
@@ -1,3 +1,4 @@
+//Código para Unity AyudaaaMe
 using System.Collections;
 using System.Collections.Generic;
 using System.IO.Ports;
@@ -5,43 +6,53 @@ using UnityEngine;
 
 public class App : MonoBehaviour
 {
-    
-    SerialPort serialport = new SerialPort("/dev/ttyACM0", 115200);
+    private SerialPort _serialPort;
+    private byte[] buffer;
+
     public string strReceived;
      
-    public string[] strData = new string[3];
-    //public string[] strData_received = new string[3];
-    //public float qw, qx, qy, qz;
+    public string[] strData = new string[4];
+    public string[] strData_received = new string[4];
+    //public float qw, qx, qy, qz; o esta
     public float x, y, z, w;
     
     private float waitTime = 0.050f;
     private float timer = 0.0f;
     void Start()
     {
-        serialport.Open(); //Open the Serial Stream.
+        _serialPort = new SerialPort();
+        _serialPort.PortName = "/dev/ttyACM0";
+        _serialPort.BaudRate =115200;
+        _serialPort.DtrEnable = true;
+        _serialPort.Open();
+        Debug.Log("Open Serial Port");
+        buffer = new byte[12];
     }
+    
     
     void Update()
     {
+        _serialPort.Write("s");
         timer += Time.deltaTime;
 
         if (timer > waitTime)
         {
             timer = timer - waitTime;
-        }
-        
-        strReceived = serialport.ReadLine(); //Read the information  
-
+            //   _serialPort.Write("ask\n");
+        //if {}
         strData = strReceived.Split(','); 
+        
         if (strData[0] != "" && strData[1] != "" && strData[2] != "")//make sure data are reday
+        //if (_serialPort.BytesToRead => 12)
         {
 
             x = float.Parse(strData[0]);
             y = float.Parse(strData[1]);
             z = float.Parse(strData[2]);
 
-            //transform.rotation = new Quaternion(-qy, -qz, qx, qw);
             //transform.rotation = new Quaternion(x, y, z, w);
+            //transform.rotation = new Quaternion(x, y, z, w);
+            transform.rotation = new Quaternion(-y, -z, x, w);
 
         }
     }
